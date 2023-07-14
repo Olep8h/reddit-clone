25b81d75b416ff60a86b6b767f5bae52be2aac4e
Olep8h <panov@student.vspj.cz>
2023-07-14 21:15:19 +0200

    start with project

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '@/styles/globals.css'
+import {Inter} from 'next/font/google'
+import {cn} from '@/lib/utils'
+
+export const metadata = {
+  title: 'Breadit',
+  description: 'A Reddit clone built with Next.js and TypeScript.',
+}
+
+
+const inter = Inter({subsets: ['latin']})
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en' className={cn('bg-white text-state-900 antialiased light',
+        inter.className)}>
+      <body className= 'min-h-screen pt-12 bg-slate-50 antialiased'>{children}</body>
+    </html>
+  )
+}
