27028681e53d47805cc465e1beb9c5fd89a4b478
Olep8h <panov@student.vspj.cz>
2023-07-16 11:56:30 +0200

    helper to know about successful authorization

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import Link from "next/link";
 import {Icons} from "@/components/Icons";
 import {buttonVariants} from "@/components/ui/Button";
+import {getAuthSession} from "@/lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+
+    const session = await getAuthSession()
+
     return (
         <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
             <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
@@ -13,10 +17,13 @@ const Navbar = () => {
                     <p className='hidden text-zinc-700 text-sm font-medium md:block'>Breadit</p>
                 </Link>
                 {/* search */}
-
-                <Link href='/sign-in' className={buttonVariants()}>
-                    Sign In
-                </Link>
+                {session ? (
+                    <p> youre logged in</p>
+                ) : (
+                    <Link href='/sign-in' className={buttonVariants()}>
+                        Sign In
+                    </Link>
+                )}
             </div>
         </div>
     )
