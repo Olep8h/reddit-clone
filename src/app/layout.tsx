bd07c8c4c7b2ad5113ec9296698f72529ad26ac2
Olep8h <panov@student.vspj.cz>
2023-07-26 17:15:50 +0200

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+ import React from 'react'
 import '@/styles/globals.css'
 import {Inter} from 'next/font/google'
 import {cn} from '@/lib/utils'
 import Navbar from '@/components/Navbar'
 import {Toaster} from "@/components/ui/Toaster";
+import Providers from "@/components/Providers";
 
 export const metadata = {
     title: 'Breadit',
@@ -14,7 +15,7 @@ export const metadata = {
 const inter = Inter({subsets: ['latin']})
 export default function RootLayout({
                                        children,
-    authModal,
+                                       authModal,
                                    }: {
     children: React.ReactNode
     authModal: React.ReactNode
@@ -23,14 +24,18 @@ export default function RootLayout({
         <html lang='en' className={cn('bg-white text-state-900 antialiased light',
             inter.className)}>
         <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-        {/* @ts-expect-error server component */}
-        <Navbar/>
+        <Providers>
 
-        {authModal}
-        <div className='container max-w-7xl mx-auto h-full pt-12'>
-            {children}
-        </div>
-        <Toaster/>
+
+            {/* @ts-expect-error server component */}
+            <Navbar/>
+
+            {authModal}
+            <div className='container max-w-7xl mx-auto h-full pt-12'>
+                {children}
+            </div>
+            <Toaster/>
+        </Providers>
         </body>
         </html>
     )
